4266ab47f843f3ec27e81ed7686cc2044439228c
Jared Scarr <jaredscarr@pm.me>
2024-05-17 14:53:17 -0700

    First commit with svelte, skeleton-ui, tailwind, and an example datatable that loads to the main page.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import '../app.css';
+	import { AppShell, AppBar, LightSwitch } from "@skeletonlabs/skeleton";
+	import { Github } from "lucide-svelte";
+
+	const currYear = new Date().getFullYear();
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">Seattle Shows</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a href="https://github.com/jaredscarr"
+					 class="btn-icon hover:bg-secondary-400-500-token"
+					 data-sveltekit-preload-data="hover">
+					<Github />
+				</a>
+				<LightSwitch />
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+	<svelte:fragment slot="footer">
+		<div class="mt-10 mb-10 text-center">
+			<p>Copyright &copy {currYear} Jared Scarr</p>
+		</div>
+	</svelte:fragment>
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Datatable from '$lib/components/Datatable.svelte';
+</script>
+<svelte:head>
+	<title>Seattle Shows</title>
+	<meta name="Displays table of a subset of live music in Seattle" content="shows.jaredscarr.com" />
+</svelte:head>
+
+<section>
+	<Datatable />
+</section>
